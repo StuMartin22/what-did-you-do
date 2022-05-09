06260fca9b4f23a0bde8478a4c57425741dc0117
Stuart (Stu) Martin <stucodes22@gmail.com>
2022-05-08 21:39:59 -0500

    added example code to reference for models.

---
+++ models/example2.js
@@ -0,0 +1,32 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connections");
+
+class Category extends Model {}
+
+Category.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    dish_category: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [3, 20],
+        isAlpha: true,
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "category",
+  }
+);
+
+module.exports = Category;
