fe352425d6da05e4dd5cf26daaa00dce5e263b68
Stuart (Stu) Martin <stucodes22@gmail.com>
2022-05-08 21:17:58 -0500

    homeroutes and userroutes were in the incorrect folders.. They're good now

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const apiRoutes = require("./api/");
-const homeRoutes = require('./api/homeroutes.js');
+const homeRoutes = require('./homeroutes.js');
 
 router.use("/api", apiRoutes);
 router.use('/', homeRoutes);
