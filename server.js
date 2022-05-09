1d8b01b19b362043e35a4ccc6f3648fbab8ae544
Stuart (Stu) Martin <stucodes22@gmail.com>
2022-05-08 20:44:29 -0500

    added base files for server initialization.

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const session = require('express-session');
+const path = require("path");
+const exphbs = require("express-handlebars");
+const sequelize = require('./config/connections');
+const router = require("./controllers");
+const bodyParser = require('body-parser');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+const hbs = exphbs.create({});
+
+const sess = {
+  secret: 'secret super secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(router);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`Now listening on ${PORT}...`));
+});
